e4adce2131f9ca6feef11211e9d419d47970b191
Daniel Cepeda <dcep93@gmail.com>
2025-03-27 01:28:06 -0400

    x

--- app/src/app/Instructions.tsx
+++ app/src/app/Instructions.tsx
@@ -3,17 +3,14 @@ export default function Instructions() {
     <div style={{ maxWidth: "50em" }}>
       <h1>Welcome to PressurePoint</h1>
       <ul>
-        <li>How many famous people can you recall with a particular name?</li>
-        <li>When you're ready, reveal the category.</li>
+        <li>How many can you name?</li>
+        <li>When you're ready, reveal the secret challenge category.</li>
         <li>Submit answers as fast as you can for better results.</li>
         <li>
           Good training for trivia! Challenge your senpai! They should add this
           to sporcle.
         </li>
-        <li>
-          Originally considered as a celebrity-namer, this could really work for
-          any category.
-        </li>
+        <li>Fan Project - no ownership intended.</li>
       </ul>
     </div>
   );
