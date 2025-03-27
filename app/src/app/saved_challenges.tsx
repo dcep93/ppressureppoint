6b5f3f1e26013989f0cff5aabb412178b091b4c4
Daniel Cepeda <dcep93@gmail.com>
2025-03-27 19:55:29 -0400

    x

--- app/src/app/saved_challenges.tsx
+++ app/src/app/saved_challenges.tsx
@@ -56,6 +56,81 @@ const saved_challenges: { [path: string]: SettingsType } = {
       },
     ],
   },
+  famous_michaels: {
+    category: "famous Michaels",
+    timer_s: 10,
+    volume: 0,
+    challenge: [
+      {
+        t: 0,
+        c: "Jordan\\https://cdn.nba.com/headshots/nba/latest/1040x760/893.png",
+      },
+      {
+        t: 0,
+        c: "B. Jordan\\https://m.media-amazon.com/images/M/MV5BMjExOTY3NzExM15BMl5BanBnXkFtZTgwOTg1OTAzMTE@._V1_FMjpg_UX1000_.jpg",
+      },
+      {
+        t: 0,
+        c: "Phelps\\https://upload.wikimedia.org/wikipedia/commons/c/c7/Michael_Phelps_Rio_Olympics_2016.jpg",
+      },
+      {
+        t: 0,
+        c: "Caine\\https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Michael_Caine_-_Viennale_2012_g_%28cropped%29.jpg/640px-Michael_Caine_-_Viennale_2012_g_%28cropped%29.jpg",
+      },
+      {
+        t: 0,
+        c: "J. Fox\\https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Michael_J_Fox_2020.jpg/640px-Michael_J_Fox_2020.jpg",
+      },
+      {
+        t: 0,
+        c: "Keaton\\https://upload.wikimedia.org/wikipedia/commons/b/b7/Michael_Keaton-63916_%28cropped%29.jpg",
+      },
+      {
+        t: 0,
+        c: "Jackson\\https://m.media-amazon.com/images/M/MV5BMTM1NjExNjg1OV5BMl5BanBnXkFtZTcwMTQ0NzIwMw@@._V1_FMjpg_UX1000_.jpg",
+      },
+      {
+        t: 0,
+        c: "Bay\\https://media.gq.com/photos/594af29ece4edb0907ff4001/16:9/w_2560%2Cc_limit/michael-bay.jpg",
+      },
+      {
+        t: 0,
+        c: "Bloomberg\\https://upload.wikimedia.org/wikipedia/commons/1/10/Mike_Bloomberg_Headshot_%283x4_cropped%29.jpg",
+      },
+      {
+        t: 0,
+        c: "Cera\\https://upload.wikimedia.org/wikipedia/commons/8/8e/Michael_Cera_2012_%28Cropped%29.jpg",
+      },
+      {
+        t: 0,
+        c: "Bublé\\https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Michael_Bubl%C3%A9_%2853695559289%29_%28cropped%29.jpg/1200px-Michael_Bubl%C3%A9_%2853695559289%29_%28cropped%29.jpg",
+      },
+      {
+        t: 0,
+        c: "Moore\\https://upload.wikimedia.org/wikipedia/commons/2/29/Michael_Moore_66%C3%A8me_Festival_de_Venise_%283x4_cropped%29.jpg",
+      },
+      {
+        t: 0,
+        c: "angelo\\https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Michelangelo_Daniele_da_Volterra_%28dettaglio%29.jpg/800px-Michelangelo_Daniele_da_Volterra_%28dettaglio%29.jpg",
+      },
+      {
+        t: 0,
+        c: "Fassbender\\https://upload.wikimedia.org/wikipedia/commons/6/63/Michael_Fassbender_by_Gage_Skidmore_2015.jpg",
+      },
+      {
+        t: 0,
+        c: "Keegan-Michael Key\\https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Keegan-Michael_Key_by_Gage_Skidmore_2.jpg/1200px-Keegan-Michael_Key_by_Gage_Skidmore_2.jpg",
+      },
+      {
+        t: 0,
+        c: "Scott\\https://miro.medium.com/v2/resize:fit:500/1*xDIevNE7HEMiJQVTYg0qDQ.png",
+      },
+      {
+        t: 0,
+        c: "Wazowski\\https://i.pinimg.com/736x/44/58/f8/4458f8ef4c263622834b745f0c077726.jpg",
+      },
+    ],
+  },
 };
 
 export default saved_challenges;
