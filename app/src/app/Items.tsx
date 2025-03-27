8e63fd17eb7f240685ebcf98035af79a45f11268
Daniel Cepeda <dcep93@gmail.com>
2025-03-27 15:12:47 -0400

    x

--- app/src/app/Items.tsx
+++ app/src/app/Items.tsx
@@ -1,74 +1,73 @@
-import { createRef, RefObject, useState } from "react";
+import { useState } from "react";
 
 const startTime = Date.now();
 
-type ItemType = { c: string; t: number; ref: RefObject<HTMLInputElement> };
-
-function setBackgroundColor(item: ItemType) {
-  item.ref.current.style.backgroundColor =
-    item.c === item.ref.current.value ? "" : "lightpink";
-}
+type ItemType = { c: string; t: number };
 
 export default function Items() {
-  const [items, update] = useState<(ItemType | null)[]>([]);
-  items.forEach((item) => item?.ref.current && setBackgroundColor(item));
+  const [items, updateItems] = useState<ItemType[]>([]);
+
+  function Item(props: { item: ItemType; i: number }) {
+    const [c, updateC] = useState(props.item.c);
+    return (
+      <form
+        onSubmit={(e) =>
+          Promise.resolve()
+            .then(() => e.preventDefault())
+            .then(() =>
+              !c
+                ? null
+                : Promise.resolve(Date.now() - startTime).then((t) =>
+                    updateItems(
+                      props.item.t === 0
+                        ? items.concat({
+                            t,
+                            c,
+                          })
+                        : items
+                            .splice(props.i, 1, { ...props.item, t, c })
+                            .slice(1)
+                            .concat(items)
+                    )
+                  )
+            )
+        }
+      >
+        <input type="submit" value="☑" disabled={props.item.c === c} />
+        <input
+          value={c}
+          onChange={(e) => updateC(e.target.value)}
+          name="item_input"
+          autoFocus={props.item.t === 0}
+          style={{
+            width: "8em",
+          }}
+        />
+        <div style={{ display: "inline-block", width: "8em" }}>
+          {props.item.t === 0 ? null : (
+            <div>
+              <span>#{props.i + 1}</span>
+              {" / "}
+              <span>{(props.item.t / 1000).toFixed(3)}</span>
+            </div>
+          )}
+        </div>
+      </form>
+    );
+  }
+
   return (
     <div>
       {items
-        .concat(null)
+        .concat({
+          c: "",
+          t: 0,
+        })
         .map((item, i) => ({ item, i }))
         .reverse()
-        .map(({ item, i }, j) => (
+        .map(({ item, i }) => (
           <div key={i}>
-            <form
-              onSubmit={(e) =>
-                Promise.resolve()
-                  .then(() => e.preventDefault())
-                  .then(
-                    () =>
-                      new FormData(e.target as HTMLFormElement).get(
-                        "item_input"
-                      ) as string
-                  )
-                  .then((c) =>
-                    !c
-                      ? null
-                      : Promise.resolve(Date.now() - startTime).then((t) =>
-                          update(
-                            item !== null
-                              ? items
-                                  .splice(i, 1, { ...item, t, c })
-                                  .slice(1)
-                                  .concat(items)
-                              : items.concat({
-                                  t,
-                                  c,
-                                  ref: createRef() as RefObject<HTMLInputElement>,
-                                })
-                          )
-                        )
-                  )
-              }
-            >
-              <input
-                ref={item?.ref}
-                name="item_input"
-                autoFocus={j === 0}
-                onChange={() => item?.ref && setBackgroundColor(item)}
-                style={{
-                  width: "8em",
-                }}
-              />
-              <div style={{ display: "inline-block", width: "8em" }}>
-                {item === null ? null : (
-                  <div>
-                    <span>#{i + 1}</span>
-                    {" / "}
-                    <span>{(item.t / 1000).toFixed(3)}</span>
-                  </div>
-                )}
-              </div>
-            </form>
+            <Item item={item} i={i} />
           </div>
         ))}
     </div>
