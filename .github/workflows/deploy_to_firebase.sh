5b7e5906c9a4427df92f64286f5210b0d5b9b856
Daniel Cepeda <dcep93@gmail.com>
2025-03-27 01:36:02 -0400

    x

--- .github/workflows/deploy_to_firebase.sh
+++ .github/workflows/deploy_to_firebase.sh
@@ -12,7 +12,9 @@ SA_KEY="$1"
 # gcloud iam service-accounts create deployer-github
 # gcloud projects add-iam-policy-binding "$GOOGLE_CLOUD_PROJECT" --member="serviceAccount:deployer-github@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com" --role="roles/firebasehosting.admin"
 # gcloud iam service-accounts keys create gac.json --iam-account "deployer-github@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com"
+# echo; echo; echo
 # cat gac.json
+# echo; echo; echo
 
 cd app
 
